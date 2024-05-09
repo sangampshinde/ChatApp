6b5bd0b3e424b3ff18475aba4e4e8feec09ae562
sangampshinde <sangampshinde@gmail.com>
2024-05-09 12:28:42 +0530

    setting user add with no encryption

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,33 +1,68 @@
+// import bcrypt from 'bcryptjs'
 import User from "../models/user.model.js";
 
-export const signup = async(req,res)=>{
-   try{
+export const signup = async (req, res) => {
+  try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
-       if (password !== confirmPassword){
-        return res.status(400).json({ error: "Passwords don't match" });
-       }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
 
-       const user = await User.findOne({ username });
+    const user = await User.findOne({ username });
 
-       if (user) {
-        return res.status(400).json({ error: "Username already exists" });
-      }
-      
-      //   hash password
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
 
-   }
-   catch(error){
+    //   hash password
+    //  const salt = await bcrypt.genSalt(10);
+		//  const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log(typeof password); // Check the type of password
+    // console.log(typeof salt);     // Check the type of salt
 
-   }
- }
+    
+    
 
-export const login =(req,res)=>{
-    res.send('loginUser');
-console.log('loginUser');
-}
+    // https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-export const logout =(req,res)=>{
-    res.send('logoutUser');
-    console.log('logoutUser');
-}
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+    await newUser.save();
+
+    if (newUser) { 
+      // Generate JWT token here
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
+  } 
+  catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("loginUser");
+  console.log("loginUser");
+};
+
+export const logout = (req, res) => {
+  res.send("logoutUser");
+  console.log("logoutUser");
+};
